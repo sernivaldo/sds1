d241feb7f066dadaf3cc72fe71107c1731ec52b3
Sernivaldo Junior <sernivaldo@gmail.com>
2020-09-16 17:49:31 -0300

    POST records

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Record.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Record.java
@@ -31,11 +31,50 @@ public class Record implements Serializable {
 	}
 
 	public Record(Long id, String name, Integer age, Instant moment) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.age = age;
 		this.moment = moment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
+	public void setGame(Game game) {
+		this.game = game;
 	}
 
 	@Override
