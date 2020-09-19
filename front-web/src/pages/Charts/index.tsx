3ed829001fba14db2571efe4de511d8fd46e60bd
Sernivaldo Junior <sernivaldo@gmail.com>
2020-09-19 18:53:02 -0300

    Configurando front-web para producao

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -21,8 +21,8 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080';
-// const BASE_URL = 'https://sds1-sernivaldo.herokuapp.com';
+// const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://sds1-sernivaldo.herokuapp.com';
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
